29e4b4a0802bc9e8e5f3f12ee74377998ef5588d
pik <alexander.maznev@gmail.com>
2015-12-28 17:33:11 +0700

    Whitespace and sort order for Dumper Middleware

--- lib/schema_plus/enums/middleware.rb
+++ lib/schema_plus/enums/middleware.rb
@@ -7,12 +7,12 @@ module SchemaPlus::Enums
         module Postgresql
 
           def after(env)
-            env.connection.enums.each do |schema, name, values|
+            env.connection.enums.sort_by {|k| k[1] }.each do |schema, name, values|
               params = [name.inspect]
               params << values.map(&:inspect).join(', ')
               params << ":schema => #{schema.inspect}" if schema != 'public'
 
-              env.initial << "create_enum #{params.join(', ')}"
+              env.initial << "  create_enum #{params.join(', ')}"
             end
           end
         end
